2f564e223bf67c42d03a110a9c947fd0dc5a9858
mochiliu3000 <mochiliu3000@gmail.com>
2017-03-14 19:47:47 +0800

    Add segmentation and multi-thread

--- preprocess.py
+++ preprocess.py
@@ -2,12 +2,19 @@ import os
 import dicom
 import numpy as np
 import pandas as pd
-import scipy.ndimage
+from scipy import ndimage
 # http://opencvpython.blogspot.com/2012/05/install-opencv-in-windows-for-python.html
 import cv2
 import matplotlib.pyplot as plt
-from skimage import measure, morphology
+from skimage import measure, morphology, filters
+from skimage.segmentation import clear_border
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from multiprocessing import Pool
+
+input_folder = "./data/sample_images"
+output_folder = "./data/d3_images_mid"
+output_seg_folder = "./data/d3_images_seg_mid"
+thread_num = 2
 
 # 1. Load Dicom files
 def load_slices(path):
@@ -59,13 +66,138 @@ def resample(image, slices, new_spacing=[1, 1, 1]):
     real_resize_factor = new_shape / image.shape
     new_spacing = spacing / real_resize_factor
     # NOTE: http://stackoverflow.com/questions/13242382/resampling-a-numpy-array-representing-an-image
-    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode="nearest")
+    image = ndimage.interpolation.zoom(image, real_resize_factor, mode="nearest")
 
     return image, new_spacing
 
 # 4. Lung Segment
-# 4.1. Region Growing
-# 4.2. Connected Component Analysis
+def largest_label_volume(im, bg=-1):
+    vals, counts = np.unique(im, return_counts=True)
+    counts = counts[vals != bg]
+    vals = vals[vals != bg]
+    if len(counts) > 0:
+	    return vals[np.argmax(counts)]
+    else:
+        return None
+
+def segment_lung_mask(image, fill_lung_structures=True):
+    
+    # not actually binary, but 1 and 2. 
+    # 0 is treated as background, which we do not want
+    binary_image = np.array(image > -320, dtype=np.int8) + 1
+    labels = measure.label(binary_image)
+    # Pick the pixel in the very corner to determine which label is air.
+    #   Improvement: Pick multiple background labels from around the patient
+    #   More resistant to "trays" on which the patient lays cutting the air 
+    #   around the person in half
+    background_label = labels[0,0,0]
+
+    #Fill the air around the person
+    binary_image[background_label == labels] = 2
+    
+    
+    # Method of filling the lung structures (that is superior to something like 
+    # morphological closing)
+    if fill_lung_structures:
+        # For every slice we determine the largest solid structure
+        for i, axial_slice in enumerate(binary_image):
+            axial_slice = axial_slice - 1
+            labeling = measure.label(axial_slice)
+            l_max = largest_label_volume(labeling, bg=0)
+            
+            if l_max is not None: #This slice contains some lung
+                binary_image[i][labeling != l_max] = 1
+
+    
+    binary_image -= 1 #Make the image actual binary
+    binary_image = 1-binary_image # Invert it, lungs are now 1
+    
+    # Remove other air pockets insided body
+    labels = measure.label(binary_image, background=0)
+    l_max = largest_label_volume(labels, bg=0)
+    if l_max is not None: # There are air pockets
+        binary_image[labels != l_max] = 0
+ 
+    return binary_image
+
+def get_segmented_lungs(im, plot=False):
+    '''
+    This funtion segments the lungs from the given 2D slice.
+    '''
+    if plot == True:
+        f, plots = plt.subplots(8, 1, figsize=(5, 40))
+    '''
+    Step 1: Convert into a binary image.
+    '''
+    binary = im < 604
+    #binary = np.array(im < 604, dtype=np.int8)
+    if plot == True:
+        plots[0].axis('off')
+        plots[0].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 2: Remove the blobs connected to the border of the image.
+    '''
+    cleared = clear_border(binary)
+    if plot == True:
+        plots[1].axis('off')
+        plots[1].imshow(cleared, cmap=plt.cm.bone)
+    '''
+    Step 3: Label the image.
+    '''
+    label_image = measure.label(cleared)
+    if plot == True:
+        plots[2].axis('off')
+        plots[2].imshow(label_image, cmap=plt.cm.bone)
+    '''
+    Step 4: Keep the labels with 2 largest areas.
+    '''
+    areas = [r.area for r in measure.regionprops(label_image)]
+    areas.sort()
+    if len(areas) > 2:
+        for region in measure.regionprops(label_image):
+            if region.area < areas[-2]:
+                for coordinates in region.coords:
+                    label_image[coordinates[0], coordinates[1]] = 0
+    binary = label_image > 0
+    if plot == True:
+        plots[3].axis('off')
+        plots[3].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 5: Erosion operation with a disk of radius 2. This operation is
+    seperate the lung nodules attached to the blood vessels.
+    '''
+    selem = morphology.disk(2)
+    binary = morphology.binary_erosion(binary, selem)
+    if plot == True:
+        plots[4].axis('off')
+        plots[4].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 6: Closure operation with a disk of radius 10. This operation is
+    to keep nodules attached to the lung wall.
+    '''
+    selem = morphology.disk(10)
+    binary = morphology.binary_closing(binary, selem)
+    if plot == True:
+        plots[5].axis('off')
+        plots[5].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 7: Fill in the small holes inside the binary mask of lungs.
+    '''
+    edges = filters.roberts(binary)
+    binary = ndimage.binary_fill_holes(edges)
+    if plot == True:
+        plots[6].axis('off')
+        plots[6].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 8: Superimpose the binary mask on the input image.
+    '''
+    get_high_vals = binary == 0
+    im[get_high_vals] = 0
+    if plot == True:
+        plots[7].axis('off')
+        plots[7].imshow(im, cmap=plt.cm.bone)
+
+    return im
 
 # 5. Normalization
 def normalize(image):
@@ -127,32 +259,57 @@ def plot_3d(image, threshold=-300):
 ############################
 ##          Util          ##
 ############################
-def preprocess(input_folder="./data/sample_images", output_folder="./d3_images"):
-    patients = os.listdir(input_folder)
-    count = 0
-    for patient in patients:
-        patient_folder = input_folder + "/" + patient
-        patient_slices = load_slices(patient_folder)
-        if patient_slices is None:
-            continue
-        patient_d3_image = convert_hu(patient_slices)
-        patient_d3_image_resample, new_spacing = resample(patient_d3_image, patient_slices)
-        print("INFO: Before resampling, patient's image shape is " + str(patient_d3_image.shape))
-        print("INFO: After  resampling, patient's image shape is " + str(patient_d3_image_resample.shape))
-        print("INFO: New spacing is " + str(new_spacing))  # [ 1.  0.9999996  0.9999996]
-        patient_d3_image_resample_clean = zero_center(normalize(patient_d3_image_resample))
-        patient_d3_image_resample_clean_resized = resize(patient_d3_image_resample_clean, 0)
-        patient_d3_image_resample_clean_resized_xyz = resize(patient_d3_image_resample_clean_resized, 1)
-        # plot_3d(patient_d3_image_resample_clean_resized, 400)  # preview 3d image
-        # plt.show()
-        # plot_3d(patient_d3_image_resample_clean_resized_new, 400)  # preview 3d image
-        # plt.show()
+def preprocess(patient):
+    print("INFO: Processing image for patient " + str(patient))
+    patient_folder = input_folder + "/" + patient
+    patient_slices = load_slices(patient_folder)
+    if patient_slices is None:
+        print("WARN: No slices for patient " + str(patient))
+        return
+    patient_d3_image = convert_hu(patient_slices)
+    patient_d3_image_resample, new_spacing = resample(patient_d3_image, patient_slices)
+    print("INFO: Before resampling, patient's image shape is " + str(patient_d3_image.shape))
+    print("INFO: After  resampling, patient's image shape is " + str(patient_d3_image_resample.shape))
+    print("INFO: New spacing is " + str(new_spacing))  # [ 1.  0.9999996  0.9999996]
+#    patient_mask_image = segment_lung_mask(first_patient_d3_image_resample)
+#    print("INFO: Generated binary image during segmentation")
+    patient_d3_image_resample_clean = zero_center(normalize(patient_d3_image_resample))
+    patient_d3_image_resample_clean_resized = resize(patient_d3_image_resample_clean, 0)
+    patient_d3_image_resample_clean_resized_xyz = resize(patient_d3_image_resample_clean_resized, 1)
+    # plot_3d(patient_d3_image_resample_clean_resized, 400)  # preview 3d image
+    # plt.show()
 
-        output_patient = output_folder + "/" + patient
-        count += 1
-        np.save(output_patient, patient_d3_image_resample_clean_resized_xyz)
-        print "saving: No." + str(count) + " patient. id: " + patient
-        print "=============================================================="
+    output_patient = output_folder + "/" + patient
+    np.save(output_patient, patient_d3_image_resample_clean_resized_xyz)
+    print("INFO: Saving ndarray of patient %s ... ..." % patient)
+    print "=============================================================="
+
+def preprocess_segment(patient):
+    print("INFO: Processing segment image for patient " + str(patient))
+    patient_folder = input_folder + "/" + patient
+    patient_slices = load_slices(patient_folder)
+    if patient_slices is None:
+        print("WARN: No slices for patient " + str(patient))
+        return
+    for inx, slice in enumerate(patient_slices):
+        patient_slices[inx].pixel_array = get_segmented_lungs(slice.pixel_array)
+    print("INFO: Applied binary mask during segmentation")
+    patient_d3_image = convert_hu(patient_slices)
+    patient_d3_image_resample, new_spacing = resample(patient_d3_image, patient_slices)
+    print("INFO: Before resampling, patient's image shape is " + str(patient_d3_image.shape))
+    print("INFO: After  resampling, patient's image shape is " + str(patient_d3_image_resample.shape))
+    print("INFO: New spacing is " + str(new_spacing))  # [ 1.  0.9999996  0.9999996]
+
+    patient_d3_image_resample_clean = zero_center(normalize(patient_d3_image_resample))
+    patient_d3_image_resample_clean_resized = resize(patient_d3_image_resample_clean, 0)
+    patient_d3_image_resample_clean_resized_xyz = resize(patient_d3_image_resample_clean_resized, 1)
+    # plot_3d(patient_d3_image_resample_clean_resized, 400)  # preview 3d image
+    # plt.show()
+
+    output_patient = output_seg_folder + "/" + patient
+    np.save(output_patient, patient_d3_image_resample_clean_resized_xyz)
+    print("INFO: Saving segmented ndarray of patient %s ... ..." % patient)
+    print "=============================================================="
 
 def decode_label(label):
     return int(label)
@@ -179,37 +336,7 @@ def observe_thickness(path):
 ##          Main          ##
 ############################
 if __name__ == '__main__':
-    input_folder = "./data/images/stage1"
-    ############## single example ############
-    # patients = os.listdir(input_folder)
-    # first_patient = patients[1]
-    # print("INFO: Fetch the 1st patient %s" % first_patient)
-    #
-    # first_patient_folder = input_folder + "/" + first_patient
-    # first_patient_slices = load_slices(first_patient_folder)
-    # first_patient_d3_image = convert_hu(first_patient_slices)
-    #
-    # first_patient_d3_image_resample, new_spacing = resample(first_patient_d3_image, first_patient_slices)
-    # print("INFO: Before resampling, 1st patient's image shape is " + str(first_patient_d3_image.shape))
-    # print("INFO: After  resampling, 1st patient's image shape is " + str(first_patient_d3_image_resample.shape))
-    # print("INFO: New spacing is " + str(new_spacing))  # [ 1.  0.9999996  0.9999996]
-    # first_patient_d3_image_resample_clean = zero_center(normalize(first_patient_d3_image_resample))
-    # first_patient_d3_image_resample_clean_resized = resize(first_patient_d3_image_resample_clean)
-    # plot_3d(first_patient_d3_image_resample, 0.5) # preview 3d image
-
-    ############## preprocess ############
-    # observe_thickness("./data/images/stage1/b8bb02d229361a623a4dc57aa0e5c485")
-    # observe_thickness("./data/images/stage1/a4dc34f2731b62f60c6c390a39fe11b2")
-
-    preprocess(input_folder, "./data/d3_images_mid")
-
-    ############## load data ############
-    # plot_3d(first_patient_d3_image_resample_clean, -3000)  # preview 3d image
-    # plt.hist(first_patient_d3_image.flatten(), bins=80, color='c')
-    # plt.show()
-    # plt.imshow(first_patient_d3_image[80], cmap=plt.cm.gray)
-    # plt.show()
-    # plt.imshow(first_patient_d3_image_resample[80], cmap=plt.cm.gray)
-    # plt.show()
-    # plt.imshow(first_patient_d3_image_resample_clean[80], cmap=plt.cm.gray)
-    # plt.show()
+    patients = os.listdir(input_folder)
+    pool = Pool(thread_num)
+    #pool.map(preprocess, patients)
+    pool.map(preprocess_segment, patients)
